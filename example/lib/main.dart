9cd3a3131057b3571b4897d6cef37dcea0bcd3c9
Aditya Nahak <adityanahak@Adityas-MacBook-Air.local>
2022-08-03 20:54:17 +0530

    UI fixes and detailed explaination of the exception

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:catcherror/catcherror.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   Catcherror.init(
     () => runApp(const MyApp()),
-    postRequestUrl: "",
-    onErrorCallBack: ((p0, p1) {}),
-    onHttpRequestFailedCallback: (p0, p1) {},
+    // postRequestUrl: "",
+    onErrorCallBack: ((p0, p1) {
+      Fluttertoast.showToast(
+          msg: p0.toString(), toastLength: Toast.LENGTH_LONG);
+    }),
+    // onHttpRequestFailedCallback: (p0, p1) {},
   );
 }
 
@@ -22,27 +26,82 @@ class _MyAppState extends State<MyApp> {
 
   void trigger() async {
     final result = await _catcherrorPlugin.checkVowel('2');
-    print("RESULT -----> $result");
+    debugPrint("RESULT -----> $result");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaleFactor: 1.0,
+          ), //set desired text scale factor here
+          child: child!,
+        );
+      },
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Catch Unhandled Native Exceptions'),
+          title: const Text('Catch Error',
+              style: TextStyle(color: Color(0xff2C3333))),
+          backgroundColor: const Color(0xffA5C9CA),
         ),
-        body: const Center(
-          child: Text(
-            "Hello this is an application to catch unhandled native exceptions.\n Please click on the action button to trigger an exception.",
-            textAlign: TextAlign.center,
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello, this is an application to catch unhandled native exceptions.\n\n Please click on the action button to trigger an exception.",
+              style: TextStyle(color: Color(0xff2C3333), fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Theme(
+              data:
+                  Theme.of(context).copyWith(dividerColor: Colors.transparent),
+              child: const ExpansionTile(
+                tilePadding: EdgeInsets.only(left: 25),
+                title: Text(
+                  "Want to know more about the exception?",
+                  style: TextStyle(
+                      color: Color(0xff395B64),
+                      fontSize: 15,
+                      decoration: TextDecoration.underline),
+                  textAlign: TextAlign.center,
+                ),
+                trailing: SizedBox(),
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      "There is a method written in native language (checkVowel) which check if the given alphabet is a vowel or not. Since we wanted an unhandled native exception, we are expecting a string in the native code, but sending an integer which causes type mismatch and triggers an exception.\n\nYou can see a toast message after click the floating action button which signifies that an exception has been occured.",
+                      style: TextStyle(color: Color(0xff2C3333), fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             trigger();
           },
-          child: const Icon(Icons.error),
+          child: Container(
+            width: 70,
+            height: 70,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: Color(0xff2C3333)),
+            child: const Icon(
+              Icons.error_outline_outlined,
+              color: Color(0xffA5C9CA),
+              size: 35,
+              // size: 45,
+            ),
+          ),
         ),
       ),
     );
